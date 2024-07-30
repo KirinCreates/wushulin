95422b6ce7b24177b95dab09371d9b862de3b77a
Lin Loi <kirin.makes@gmail.com>
2024-07-30 15:13:42 +0200

    adding svelte utteranc.es for comments

---
+++ src/routes/blog/+layout.svelte
@@ -0,0 +1,12 @@
+<slot />
+
+<p>Kommentarer</p>
+<script>
+    import { Utterances, utterancesTheme } from "@codewithshin/svelte-utterances";
+    const theme = 'github-light';
+    let reponame = "KirinCreates/wushulin"
+</script>
+
+<div>
+    <Utterances {reponame} {theme} />
+</div>
