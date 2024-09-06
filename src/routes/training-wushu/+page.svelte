1d1933a439b8ef7236c543a31738a6a12ed0ecb0
Lin Loi <kirin.makes@gmail.com>
2024-09-06 15:37:32 +0200

    adding more info about training

--- src/routes/training-wushu/+page.svelte
+++ src/routes/training-wushu/+page.svelte
@@ -4,21 +4,24 @@
     <div class="mx-1 mb-6">
         <iframe class="w-full aspect-[9/16]" src="https://youtube.com/embed/HNytzq6FXMI?si=S9G9HRY5z5akCcP1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
-
+    
     <p class="my-4 md:my-6 mx-1">
-        Alla intresserade av att börja träna wushu kan kontakta mig i förväg via kirin.makes'at'gmail.com (ersätt 'at' med @) med ämnesraden 
+        Enklast når ni mig via <a target="_blank" rel="noopener noreferrer" href="https://m.me/linloi1337" class="text-sky-500 no-underline hover:underline ...">facebook messenger</a>. Bör då kunna skicka privatmeddelanden
+    </p>
+    <p class="my-4 md:my-6 mx-1">
+        Vill man inte nå mig via messenger så kan man också kontakta mig i förväg via kirin.makes'at'gmail.com (ersätt 'at' med @) med ämnesraden 
         träna wushu och inkludera ett innehåll där du skriver ditt för- och efternamn, din epost, födelsedatum, telefonnummer samt adress. Är du under
         18 år så behöver du vårdnadshavares godkännande. Vårdnadshavare behöver skicka ett mail till mig och utöver ovanstående lägga 
         till vårdnashavarens kontaktuppgifter (för- och efternamn, e-post, telefonnummer samt adress).
     </p>
 
     <p class="my-4 md:my-6 mx-1">
-        Ni kan också nå mig via facebook. Se facebook-ikonen längst upp på hemsidan.
+        Ni kan också nå mig via min publika facebook-grupp. Se facebook-ikonen längst upp på hemsidan.
     </p>
 
     <p class="my-4 md:my-6 mx-1">
         Träningen kommer att hållas i Brotorpshallen måndagar kl. 18-19. 
-        Maila i förväg om du är ny eller kontakta mig via facebook (se ovan) så vet jag att du kommer.
+        Maila i förväg om du är ny eller kontakta mig via facebook Messenger (se ovan) så vet jag att du kommer.
     </p>
     <div class="mx-1 mb-6">
         <iframe class="w-full aspect-square" src="https://www.google.com/maps/d/u/0/embed?mid=173PJKfYG1glyvrLf2TmMba8IqiTNRq0&ehbc=2E312F"></iframe>
