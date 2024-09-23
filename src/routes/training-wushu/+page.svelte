8c53da9f2a4de0bfefac4132a6d81e57b7197887
Lin Loi <kirin.makes@gmail.com>
2024-09-23 14:11:00 +0200

    updating training-wushu info

--- src/routes/training-wushu/+page.svelte
+++ src/routes/training-wushu/+page.svelte
@@ -1,8 +1,12 @@
 <div class="md:w-[48rem] md:mx-auto md:text-lg lg:text-xl">
     <h1 class="text-3xl font-bold my-4 md:my-6 mx-1">Börja träna kungfu/wushu</h1>
-    
+    <p class="my-4 md:my-6 mx-1">
+        Plats: Gräsmattan bredvid Brotorpshallen, Sundbyberg. <br>
+        Tid: kl. 18-19 <br>
+        Pris: Ett gratispass. Efter det 1200 kr för ett pass i veckan (sista träningsveckan för terminen: v.50).
+    </p>
     <div class="mx-1 mb-6">
-        <iframe class="w-full aspect-[9/16]" src="https://youtube.com/embed/HNytzq6FXMI?si=S9G9HRY5z5akCcP1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe class="w-full aspect-[9/16]" src="https://youtube.com/embed/9olxaPMV0v4?si=rShqEN8M6IFC1iHw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
 
     <div class="mx-1 mb-6">
@@ -10,13 +14,14 @@
     </div>
     
     <p class="my-4 md:my-6 mx-1">
-        Enklast når ni mig via <a target="_blank" rel="noopener noreferrer" href="https://m.me/linloi1337" class="text-sky-500 no-underline hover:underline ...">facebook messenger</a>. Då kan ni skicka privatmeddelanden till mig.
+        Enklast når ni mig via <a target="_blank" rel="noopener noreferrer" href="https://m.me/linloi1337" class="text-sky-500 no-underline hover:underline ...">facebook messenger</a>. Då kan ni skicka privatmeddelanden till mig om ni undrar något.
     </p>
     <p class="my-4 md:my-6 mx-1">
         Vill man inte nå mig via messenger så kan man också kontakta mig i förväg via kirin.makes'at'gmail.com (ersätt 'at' med @) med ämnesraden 
-        träna wushu och inkludera ett innehåll där du skriver ditt för- och efternamn, din epost, födelsedatum, telefonnummer samt adress. Är du under
+        träna wushu. 
+        <!-- och inkludera ett innehåll där du skriver ditt för- och efternamn, din epost, födelsedatum, telefonnummer samt adress. Är du under
         18 år så behöver du vårdnadshavares godkännande. Vårdnadshavare behöver skicka ett mail till mig och utöver ovanstående lägga 
-        till vårdnashavarens kontaktuppgifter (för- och efternamn, e-post, telefonnummer samt adress).
+        till vårdnashavarens kontaktuppgifter (för- och efternamn, e-post, telefonnummer samt adress). -->
     </p>
 
     <p class="my-4 md:my-6 mx-1">
@@ -24,10 +29,9 @@
     </p>
 
     <p class="my-4 md:my-6 mx-1">
-        Träningen kommer att hållas i Brotorpshallen måndagar kl. 18-19. 
         Maila i förväg om du är ny eller kontakta mig via facebook Messenger (se ovan) så vet jag att du kommer.
     </p>
-    <div class="mx-1 mb-6">
+    <!-- <div class="mx-1 mb-6">
         <iframe class="w-full aspect-square" src="https://www.google.com/maps/d/u/0/embed?mid=173PJKfYG1glyvrLf2TmMba8IqiTNRq0&ehbc=2E312F"></iframe>
-    </div>
+    </div> -->
 </div>
