9cd752f1ef0ca6bbdd7ca4a78e75be95fcc2dea8
Lin Loi <kirin.makes@gmail.com>
2024-10-07 10:45:49 +0200

    fixing video

--- src/routes/blog/training-20241005/+page.svelte
+++ src/routes/blog/training-20241005/+page.svelte
@@ -11,7 +11,7 @@
     <p class="my-4 md:my-6 mx-1"><em>Datum skrivet: 2024-10-07</em></p>
     <p class="my-4 md:my-6 mx-1">Varit lite sjuk så inte laddat upp på ett tag men nu är man friskare och det var ett tag sedan som man körde lite hoppsparkar. Video på träning:</p>
     <div class="mx-1 mb-6">
-        <iframe class="w-full aspect-[9/16]" src="https://youtu.be/t-3j_JHOw28?si=CyhzPnUl-hdZUrJb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe class="w-full" src="https://youtube.com/embed/t-3j_JHOw28?si=CyhzPnUl-hdZUrJb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
     <p class="my-4 md:my-6 mx-1">Följ mig gärna på instagram eller prenumerera på min youtube-kanal. Se menyn ovan.</p>
 
