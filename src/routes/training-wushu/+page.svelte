36812f3100e69ffdbaae2980342cec50a95c5de4
KirinCreates <kirin.makes@gmail.com>
2026-07-17 09:18:21 +0200

    updating term start

--- src/routes/training-wushu/+page.svelte
+++ src/routes/training-wushu/+page.svelte
@@ -2,12 +2,12 @@
     <h1 class="text-3xl font-bold my-4 md:my-6 mx-1">Börja träna kungfu/wushu</h1>
     <p class="my-4 md:my-6 mx-1">
         <b>Plats</b>: Fridensborgsvägen 64, Solna (Järvastaden). Bredvid Nära dej Fridens livs. <br>
-        <b>Terminsstart och Tider</b>: Terminsstart 18/1. Fredagar kl. 18.15-19.15 och söndagar kl. 10-11.30.<br>
-        <b>Terminsslut</b>: Söndagen den 7/6 v. 23. <br>
-        <b>Pris</b>: Ett gratispass. Efter det 1800 kr/person för två pass i veckan. <br>
-        <b>Ingen träning</b> söndagen den 22/2, v. 8 och under sportlovsveckan v. 9. <br>
-        <b>Ingen träning</b> söndagen den 5/4, v. 14 och under påsklovsveckan v. 15. <br>
-        <b>Betalning</b>: Kontakta Lin via epost eller messenger så fakturerar jag. Ange Meddelande vid kontakt: "namn+personnummer" (12 siffror) för varje person som tränar. Separera flera personer med kommatecken. <br>
+        <b>Åldersgräns</b>: 9 år, men har man god koncentration (kan komma ihåg samt följa instruktioner) och koordination (kroppskontroll + kan skilja på höger och vänster samt härma rörelser) så går det att tumma på denna regel.<br>
+        <b>Terminsstart och Tider</b>: Terminsstart: söndagen den 6/9 v. 36. Vi kör Fredagar kl. 18.15-19.15 och söndagar kl. 10-11.30.<br>
+        <b>Terminsslut</b>: Fredagen den 11/12 v. 50. <br>
+        <b>Pris</b>: Ett gratis prova-på-pass. Vill man sedan fortsätta så kostar det 1800 kr/person för två pass i veckan. <br>
+        <b>Ingen träning</b> söndagen den 25/10, v. 43 och under höstlovsveckan v. 44. <br>
+        <b>Betalning</b>: Swish. Kontakta Lin på träningen eller via epost eller messenger så får ni mitt nummer. Vid betalning ange namn och personummer för alla deltagare som betalningen avser. <br>
         Ingen återbetalning av träningsavgift sker.
     </p>
     <div class="mx-1 mb-6">
@@ -40,3 +40,14 @@
         <iframe class="w-full aspect-square" src="https://www.google.com/maps/d/u/0/embed?mid=173PJKfYG1glyvrLf2TmMba8IqiTNRq0&ehbc=2E312F"></iframe>
     </div> -->
 </div>
+
+<!-- <p class="my-4 md:my-6 mx-1">
+    <b>Plats</b>: Fridensborgsvägen 64, Solna (Järvastaden). Bredvid Nära dej Fridens livs. <br>
+    <b>Terminsstart och Tider</b>: Terminsstart 18/1. Fredagar kl. 18.15-19.15 och söndagar kl. 10-11.30.<br>
+    <b>Terminsslut</b>: Söndagen den 7/6 v. 23. <br>
+    <b>Pris</b>: Ett gratispass. Efter det 1800 kr/person för två pass i veckan. <br>
+    <b>Ingen träning</b> söndagen den 22/2, v. 8 och under sportlovsveckan v. 9. <br>
+    <b>Ingen träning</b> söndagen den 5/4, v. 14 och under påsklovsveckan v. 15. <br>
+    <b>Betalning</b>: Kontakta Lin via epost eller messenger så fakturerar jag. Ange Meddelande vid kontakt: "namn+personnummer" (12 siffror) för varje person som tränar. Separera flera personer med kommatecken. <br>
+    Ingen återbetalning av träningsavgift sker.
+</p> -->
